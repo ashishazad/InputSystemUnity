a1cc64efa5b580811c9d7cbfe2bfe84847c2ef54
ashishazad <ashishazad1998@gmail.com>
2023-04-15 11:40:13 +0530

    Initial commit

---
+++ Assets/InputSystem/TestingInputSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestingInputSystem : MonoBehaviour
+{
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+    PlayerInputActions playerInputActions;
+
+    [SerializeField] float speed = 10;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        //playerInput.onActionTriggered += PlayerInput_onActionTriggered;//for c# events
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+        //playerInputActions.Player.Movement.performed += Movement;
+    }
+
+    private void Update()
+    {
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
+    }
+    //private void Movement(InputAction.CallbackContext context)
+    //{
+    //    Debug.Log(context);
+    //    Vector2 movement = context.ReadValue<Vector2>();
+    //    rb.AddForce(new Vector3(movement.x,0,movement.y) * speed, ForceMode.Force);
+    //}
+
+    private void PlayerInput_onActionTriggered(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+        if (context.phase == InputActionPhase.Performed)
+        {
+            rb.velocity = new Vector3(0, 5, 0);
+        }
+    }
+}
